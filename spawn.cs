ebf8eedb836e31297221e93c91c95e0793dc4e76
Edwin Kimsal <kimsal.edwin@gmail.com>
2026-05-08 11:24:12 -0400

    Add files via upload

--- spawn.cs
+++ spawn.cs
@@ -21,6 +21,9 @@ public class spawn : MonoBehaviour
     // Public variable of number of balls
     public int num_balls;
 
+    // Public variable of teaching mode
+    public bool teaching_mode;
+
     // Set pos vars and bool
     private int x_pos;
     private int y_pos;
@@ -36,43 +39,78 @@ public class spawn : MonoBehaviour
         types.Add(3, Tuple.Create(90, new Color32(94, 255, 110, 255), 4));
         types.Add(4, Tuple.Create(100, new Color32(138, 255, 244, 255), 5));
 
-        // Iterate for each of the number of balls
+        // ----------------------------
+        // TEACHING MODE (minimal)
+        // ----------------------------
+        if (teaching_mode)
+        {
+            int offset = 400;
+
+            // Pick ONE random type → both balls use it
+            int key = rnd.Next(0, 5);
+
+            int size = types[key].Item1;
+            Color32 color = types[key].Item2;
+            int mass = types[key].Item3;
+
+            // Random center position
+            int randX = rnd.Next(-200, 201);
+            int randY = rnd.Next(-200, 201);
+
+            // Random speed (but same magnitude)
+            int speed1 = rnd.Next(60, 126);
+            int speed2 = rnd.Next(-126, -61);
+
+            // Spawn left ball
+            GameObject b1 = Instantiate(circle, new Vector3(randX - offset, randY), Quaternion.identity);
+            Rigidbody rb1 = b1.GetComponent<Rigidbody>();
+            rb1.velocity = new Vector3(speed1, 0, 0);
+
+            // Spawn right ball
+            GameObject b2 = Instantiate(circle, new Vector3(randX + offset, randY), Quaternion.identity);
+            Rigidbody rb2 = b2.GetComponent<Rigidbody>();
+            rb2.velocity = new Vector3(speed2, 0, 0);
+
+            // Apply SAME struct properties to both
+            foreach (GameObject b in new GameObject[] { b1, b2 })
+            {
+                b.transform.localScale = new Vector2(size, size);
+                b.GetComponent<Rigidbody>().mass = mass;
+                b.GetComponent<Renderer>().material.color = color;
+            }
+
+            return; // skip normal spawning
+        }
+
+        // ----------------------------
+        // NORMAL MODE (unchanged)
+        // ----------------------------
         for (int i = 0; i < num_balls; i++)
         {
-            // is_valid is, set is_valid to true by default
             bool is_valid = false;
 
-            // Find a working coord until valid
             while (is_valid == false)
             {
-                // Set margin
-                int margin = 30; // Used for boundry between obj
+                int margin = 30;
 
-                // Find a random point
-                x_pos = rnd.Next(-1 * (int)front.transform.localScale.x / 2 + types[4].Item1/2 + margin, (int)front.transform.localScale.x / 2 + 1 - types[4].Item1/2 - margin);
-                y_pos = rnd.Next(-1 * (int)front.transform.localScale.y / 2 + types[4].Item1/2 + margin, (int)front.transform.localScale.y / 2 + 1 - types[4].Item1/2 - margin);
+                x_pos = rnd.Next(-1 * (int)front.transform.localScale.x / 2 + types[4].Item1 / 2 + margin, (int)front.transform.localScale.x / 2 + 1 - types[4].Item1 / 2 - margin);
+                y_pos = rnd.Next(-1 * (int)front.transform.localScale.y / 2 + types[4].Item1 / 2 + margin, (int)front.transform.localScale.y / 2 + 1 - types[4].Item1 / 2 - margin);
 
-                // If no obj, it must be false
                 if (circle_arr == null)
                 {
                     is_valid = true;
                 }
-
-                // Else contunue
                 else
                 {
-                    // Set is_true
                     int is_true = 1;
 
-                    // Iterate through each circle already created
                     foreach (GameObject obj in circle_arr)
                     {
-                        // Get point of this obj
                         int x_other_obj = (int)obj.transform.position.x;
                         int y_other_obj = (int)obj.transform.position.y;
 
-                        // Make sure new point is valid
-                        if (Math.Abs(x_pos - x_other_obj) <= types[4].Item1/2 + obj.transform.localScale.x/2 + margin && Math.Abs(y_pos - y_other_obj) <= types[4].Item1/2 + obj.transform.localScale.y/2 + margin)
+                        if (Math.Abs(x_pos - x_other_obj) <= types[4].Item1 / 2 + obj.transform.localScale.x / 2 + margin &&
+                            Math.Abs(y_pos - y_other_obj) <= types[4].Item1 / 2 + obj.transform.localScale.y / 2 + margin)
                         {
                             is_true = 0;
                         }
@@ -85,10 +123,7 @@ public class spawn : MonoBehaviour
                 }
             }
 
-            // Spawn a ball
-            spawn_ball(x_pos, y_pos, types); // (x-coor, y-coor, dict of types)
-
-            // Get array of all spawned balls
+            spawn_ball(x_pos, y_pos, types);
             circle_arr = GameObject.FindGameObjectsWithTag("circle");
         }
     }
